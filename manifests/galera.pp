ec94d25b926a42c4a496cd43fa7309fc910a7843
Anshu Prateek <anshu.prateek@ril.com>
2015-12-06 22:11:00 +0530

    Fixing galera manifest and changing default to replica

--- manifests/galera.pp
+++ manifests/galera.pp
@@ -4,14 +4,26 @@
 #
 
 class rjil::galera(
-    $dummy_arg = 'dummyarg',
+    $galera_role  = 'replica',
+    $bind_address = 'localhost',
 ) {
-    $galera_servers = values(service_discover_consul('mysql'))
-    $galera_master = values(service_discover_consul('mysql','master'))
-    class { '::galera':
-        galera_servers     => $galera_servers,
-        galera_master      => $galera_master,
-        vendor_type        => 'mariadb',
-        configure_firewall => false,
-    }
+  ## Setup test code
+
+  rjil::test { 'mysql.sh': }
+
+  $galera_servers = values(service_discover_consul('mysql'))
+  $galera_master = values(service_discover_consul('mysql','master'))
+  class { '::galera':
+	        galera_servers     => $galera_servers,
+	        galera_master      => $galera_master,
+	        vendor_type        => 'mariadb',
+	        configure_firewall => false,
+        }
+
+  rjil::jiocloud::consul::service { "mysql":
+    tags          => [$galera_role],
+    port          => 3306,
+    check_command => "/usr/lib/nagios/plugins/check_mysql -H ${bind_address} -u monitor -p monitor"
+  }
+
 }
