d473373af14bb0fb57d834c3a87490b59befe1aa
Anshu Prateek <anshu.prateek@ril.com>
2015-12-29 10:57:53 +0530

    Adding test for pacemaker + fixing tab/spaces
    Changes as follow:
    	modified:   manifests/pacemaker.pp
    Added pacemaker service/validation test
    	new file:   templates/tests/pacemaker_check.sh.erb
    Check file for above service validation.
    This presently does a simple service pacemaker status check

--- manifests/pacemaker.pp
+++ manifests/pacemaker.pp
@@ -13,23 +13,27 @@ class rjil::pacemaker(
 
   $unicast_addresses = values(service_discover_consul('haproxy', 'global'))
 
-	class { 'corosync':
-	  enable_secauth    => $enable_secauth,
-	  authkey           => $authkey,
-	  bind_address      => $ipaddress,
-	  unicast_addresses => $unicast_addresses,
-	  quorum_members    => $unicast_addresses,
-	}
+  class { 'corosync':
+    enable_secauth    => $enable_secauth,
+    authkey           => $authkey,
+    bind_address      => $ipaddress,
+    unicast_addresses => $unicast_addresses,
+    quorum_members    => $unicast_addresses,
+  }
 
-	corosync::service { 'pacemaker':
+  corosync::service { 'pacemaker':
     version => '0',
   }
 
-	cs_primitive { 'haproxy_vip':
-	  primitive_class => 'ocf',
-	  primitive_type  => 'IPaddr2',
-	  provided_by     => 'heartbeat',
-	  parameters      => { 'ip' => $haproxy_vip_ip, 'cidr_netmask' => $haproxy_vip_ip_netmask, 'nic' =>$haproxy_vip_nic },
-	  operations      => { 'monitor' => { 'interval' => $haproxy_vip_monitor_interval } },
-	}
+  cs_primitive { 'haproxy_vip':
+    primitive_class => 'ocf',
+    primitive_type  => 'IPaddr2',
+    provided_by     => 'heartbeat',
+    parameters      => { 'ip' => $haproxy_vip_ip, 'cidr_netmask' => $haproxy_vip_ip_netmask, 'nic' =>$haproxy_vip_nic },
+    operations      => { 'monitor' => { 'interval' => $haproxy_vip_monitor_interval } },
+  }
+
+  rjil::test::check { 'pacemaker':
+  }
+
 }
