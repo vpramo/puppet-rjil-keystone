539c8dfef634989b67c9dfbec120c700f03e46f8
Anshu Prateek <anshu.prateek@ril.com>
2015-12-31 11:21:51 +0530

    Adding IP for staging + some non-related changes
    	modified:   hiera/data/env/stgiam.yaml
    Added staging VIP
    	modified:   manifests/openstack_extras/keystone_endpoints.pp
    Enabled admin creation only, disabled endpoint creation as thats failing presently
    	modified:   manifests/openstack_objects.pp
    Enabling keystone_endpoint.pp + runtime_fails

--- manifests/openstack_extras/keystone_endpoints.pp
+++ manifests/openstack_extras/keystone_endpoints.pp
@@ -6,6 +6,11 @@
 class rjil::openstack_extras::keystone_endpoints(
     $dummy_arg = 'dummy_arg'
 ){
-  include ::keystone::endpoint
+# TODO: Fix this!!
+# Could not prefetch keystone_endpoint provider 'openstack':
+# Execution of '/usr/bin/openstack endpoint list --quiet --format csv --long' returned 2: usage: 
+# openstack endpoint list [-h] [-f {csv,html,json,table,yaml}]
+
+#  include ::keystone::endpoint
   include ::keystone::roles::admin
 }
